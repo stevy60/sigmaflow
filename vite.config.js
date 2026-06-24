e16613af15daae3e120e9a3c1bd4e6c68b63228c
stevy60 <namanyasteve28@gmail.com>
2026-06-24 23:40:47 +0300

    Replace HTML template with Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SigmaFlow</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/main.jsx"></script>
-  </body>
-</html>
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({ plugins: [react()] })
